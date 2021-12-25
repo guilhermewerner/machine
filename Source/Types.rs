0d82ea167363258b1b8c9c73f04453dbbc334334
Werner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-12-25 08:39:36 -0300

    Update Types.rs

--- Source/Types.rs
+++ Source/Types.rs
@@ -3,22 +3,3 @@ pub type Half = [Byte; 2];
 pub type Word = [Byte; 4];
 pub type DWord = [Byte; 8];
 pub type QWord = [Byte; 16];
-
-pub enum PrimitiveType {
-    Byte,
-    Bool,
-    Char,
-    Int8,
-    Int16,
-    Int32,
-    Int64,
-    Int128,
-    UInt8,
-    UInt16,
-    UInt32,
-    UInt64,
-    UInt128,
-    Float32,
-    Float64,
-    String,
-}
